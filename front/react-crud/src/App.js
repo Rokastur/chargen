8174ad6038e7d3ba7ea92431ebb57c6e2f5bb973
rokastur <rokasatminas@gmail.com>
2023-12-30 03:42:38 +0200

    test commit for front

--- front/react-crud/src/App.js
+++ front/react-crud/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          chargenasda
+          chargenasda adas
         </a>
       </header>
     </div>
